29de9773acd126af4b7c72f48c6cdb1f7ed58658
Ashish <ashish.mishra0717@gmail.com>
2021-09-24 00:17:52 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React,{useState} from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+
+// const state = useState();
+//console.log(state);
+
+const [count,setCount] = useState(0);
+
+  const IncNum = () => {
+    setCount(count+1);
+    // console.log('clicked'+count++);
+  }
+  const DecNum = () => {
+    setCount(count-1);
+    // console.log('clicked'+count-1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <div className = "App">
+     <h2>React Count</h2>
+   <h1> {count} </h1>
+    <button onClick = {IncNum}> Increment</button>
+    <button  onClick = {DecNum}> Decrement</button> 
+   </div>
+  )
 }
 
 export default App;
